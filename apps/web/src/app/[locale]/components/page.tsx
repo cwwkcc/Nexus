9dd85eac5a4a4e64a949f94908923198909d0579
Chamila Roshana <cinderax@icloud.com>
2026-05-11 10:48:41 +0530

    feat(demo): update UI showcase with new atoms

--- apps/web/src/app/[locale]/components/page.tsx
+++ apps/web/src/app/[locale]/components/page.tsx
@@ -1,14 +1,52 @@
 'use client';
 
-import { Button } from 'packages/ui/src/components/ui/Button';
+import { Button } from 'packages/ui/src/components/atoms/Button';
+import { Input } from 'packages/ui/src/components/atoms/Input';
+import { Spinner } from 'packages/ui/src/components/atoms/Spinner';
 
 export default function Components() {
   return (
     <>
-      <h1>Buttons</h1>
-      <Button variant="primary">Click me</Button>
-      <Button variant="secondary">Click me</Button>
-      <Button variant="ghost">Click me</Button>
+      <h2>Buttons</h2>
+      <div>
+        <Button variant="primary" size="md">
+          Click me
+        </Button>
+        <Button variant="secondary" size="md">
+          Click me
+        </Button>
+        <Button variant="ghost" size="md">
+          Click me
+        </Button>
+        <Button variant="primary" size="sm">
+          Click me
+        </Button>
+        <Button variant="secondary" size="sm">
+          Click me
+        </Button>
+        <Button variant="ghost" size="sm">
+          Click me
+        </Button>
+        <Button variant="primary" size="lg">
+          Click me
+        </Button>
+        <Button variant="secondary" size="lg">
+          Click me
+        </Button>
+        <Button variant="ghost" size="lg">
+          Click me
+        </Button>
+      </div>
+
+      <h2>Input</h2>
+      <div>
+        <Input></Input>
+      </div>
+
+      <h2>Spinner</h2>
+      <div>
+        <Spinner></Spinner>
+      </div>
     </>
   );
 }
