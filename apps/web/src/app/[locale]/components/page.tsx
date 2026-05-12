c1b0ecfae30783b0499c3cdd28b6c36244150886
Chamila Roshana <cinderax@icloud.com>
2026-05-12 14:11:20 +0530

    feat(web): update component demo with new form primitives

--- apps/web/src/app/[locale]/components/page.tsx
+++ apps/web/src/app/[locale]/components/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
-import { Button } from 'packages/ui/src/components/atoms/Button';
-import { Input } from 'packages/ui/src/components/atoms/Input';
-import { Spinner } from 'packages/ui/src/components/atoms/Spinner';
+import {
+  Checkbox,
+  Radio,
+  Toggle,
+  Badge,
+  Button,
+  Input,
+  Spinner,
+  Textarea,
+} from 'packages/ui/src';
 
 export default function Components() {
   return (
     <>
-      <h2>Buttons</h2>
+      <h2>Atoms</h2>
+      <h3>Buttons</h3>
       <div>
         <Button variant="primary" size="md">
           Click me
@@ -38,14 +46,37 @@ export default function Components() {
         </Button>
       </div>
 
-      <h2>Input</h2>
+      <h3>Input</h3>
       <div>
-        <Input></Input>
+        <Input label="Username" />
       </div>
 
-      <h2>Spinner</h2>
+      <h3>Spinner</h3>
       <div>
         <Spinner></Spinner>
+      </div>
+
+      <h3>Badge</h3>
+      <div>
+        <Badge variant="category" label="Category" />
+        <Badge variant="status" status="published" />
+        <Badge variant="achievement" label="Achievement" />
+      </div>
+      <h3>Textarea</h3>
+      <div>
+        <Textarea label="Description" />
+      </div>
+      <h3>Checkbox</h3>
+      <div>
+        <Checkbox label="I agree to the terms and conditions" />
+      </div>
+      <h3>Radio</h3>
+      <div>
+        <Radio value="Value" label="Lable" name="radio-group" />
+      </div>
+      <h3>Toggle</h3>
+      <div>
+        <Toggle label="Enable notifications" />
       </div>
     </>
   );
