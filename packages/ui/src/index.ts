67696e82ae0ea53802683c251672dbca8fb352ce
Chamila Roshana <cinderax@icloud.com>
2026-05-14 08:30:54 +0530

    chore(ui): export new loader components from UI package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -10,10 +10,14 @@ export { InlineLink } from './components/atoms/InlineLink';
 export { Input } from './components/atoms/Input';
 export { Radio } from './components/atoms/Radio';
 export { Select } from './components/atoms/Select';
-export { Spinner } from './components/atoms/Spinner';
 export { Tag } from './components/atoms/Tag';
 export { Textarea } from './components/atoms/Textarea';
 export { Toggle } from './components/atoms/Toggle';
 export { ToolTip } from './components/atoms/ToolTip';
+
+// Atoms - Spinners
+export { BeatLoader } from './components/atoms/Spinners/BeatLoader';
+export { BarLoader } from './components/atoms/Spinners/BarLoader';
+export { ScaleLoader } from './components/atoms/Spinners/ScaleLoader';
 // Hooks
 export { useFormField } from './hooks/useFormField';
