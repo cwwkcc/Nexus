c92f5cad31d68dfb163c8c6c0ca316a177dcc229
Chamila Roshana <cinderax@icloud.com>
2026-05-14 08:31:04 +0530

    feat(web): showcase new loader components in demo page

--- apps/web/src/app/[locale]/components/page.tsx
+++ apps/web/src/app/[locale]/components/page.tsx
@@ -1,11 +1,18 @@
 'use client';
 
-import { Button, Spinner } from 'packages/ui/src';
-
+import { BeatLoader, BarLoader, ScaleLoader } from 'packages/ui/src';
 export default function Components() {
   return (
     <>
-      <Spinner size="md" label="Loading..." />
+      <h1>Components</h1>
+      <h2>Atoms</h2>
+      <h3>Spinners</h3>
+      <p>BarLoader</p>
+      <BarLoader size="md" variant="green" speed="normal" /> <br />
+      <p>BeatLoader</p>
+      <BeatLoader size="md" variant="gold" speed="normal" /> <br />
+      <p>ScaleLoader</p>
+      <ScaleLoader size="md" variant="muted" speed="normal" /> <br />
     </>
   );
 }
