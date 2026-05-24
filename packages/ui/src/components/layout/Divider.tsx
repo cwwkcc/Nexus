7fc3048199d90a78e35a8e6018f2a240bd1c61cc
Chamila Roshana <cinderax@icloud.com>
2026-05-24 12:08:59 +0530

    refactor(layout): enhance Divider variants, styling and accessibility

--- packages/ui/src/components/layout/Divider.tsx
+++ packages/ui/src/components/layout/Divider.tsx
@@ -1,22 +1,52 @@
-import clsx from 'clsx';
+import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-export type DividerVariant = 'horizontal' | 'gold-accent';
 
-export interface DividerProps {
-  variant?: DividerVariant;
+type Axis = 'horizontal' | 'vertical';
+type DividerAccentVariant = 'gold-accent' | 'gold-accent-short' | 'muted';
+
+interface DividerProps {
+  accentVariant?: DividerAccentVariant;
+  axis?: Axis;
   className?: string;
 }
 
-export function Divider({ variant = 'horizontal', className }: DividerProps) {
-  if (variant === 'gold-accent') {
-    return (
-      <hr
-        className={twMerge(
-          clsx('border-gold-base flex justify-center mx-48', className),
-        )}
-      />
-    );
-  } else {
-    return <hr className={twMerge(clsx('flex justify-center ', className))} />;
-  }
+const axisStyles: Record<Axis, string> = {
+  horizontal: 'w-size-full h-size-0p5',
+  vertical: 'h-size-full w-size-0p5',
+};
+
+const colorStyles: Record<DividerAccentVariant, string> = {
+  'gold-accent': 'bg-gold-base',
+  'gold-accent-short': 'bg-gold-base w-size-20 mx-auto',
+  muted: 'bg-border-light',
+};
+
+const marginStyles: Record<Axis, string> = {
+  horizontal: 'my-space-4',
+  vertical: 'mx-space-4',
+};
+
+export function Divider({
+  axis = 'horizontal',
+  accentVariant = 'muted',
+  className,
+}: DividerProps) {
+  const AxisStyles =
+    axis === 'horizontal' ? axisStyles.horizontal : axisStyles.vertical;
+
+  return (
+    <div
+      className={twMerge(
+        clsx(
+          'border-none rounded-full',
+          AxisStyles,
+          colorStyles[accentVariant],
+          marginStyles[axis],
+          className,
+        ),
+      )}
+      role="separator"
+      aria-orientation={axis}
+    />
+  );
 }
