5260c8cd8c170417847a01c1b0704e479d409143
Chamila Roshana <cinderax@icloud.com>
2026-06-22 09:27:16 +0530

    style(ui): add blank line in form component imports (part 1)

--- packages/ui/src/components/forms/Checkbox.tsx
+++ packages/ui/src/components/forms/Checkbox.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { forwardRef, useId, type ChangeEventHandler } from 'react';
+
 import { cn } from '../../utilities/cn';
 
 type Props = {
--- packages/ui/src/components/forms/FileUploadZone.tsx
+++ packages/ui/src/components/forms/FileUploadZone.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState, useRef, useCallback } from 'react';
+
 import { cn } from '../../utilities/cn';
 
 export interface FileUploadZoneProps {
