2462272fb96b3f01aab41f0f0f997e83aa7cdaeb
Chamila Roshana <cinderax@icloud.com>
2026-08-08 08:44:21 +0530

    fix(ui): allow date and time types in Input component

--- packages/ui/src/components/forms/Input.tsx
+++ packages/ui/src/components/forms/Input.tsx
@@ -10,7 +10,7 @@ type Props = {
   disabled?: boolean;
   required?: boolean;
   placeholder?: string;
-  type?: 'text' | 'email' | 'tel' | 'password' | 'number' | 'search' | 'url';
+  type?: 'text' | 'email' | 'tel' | 'password' | 'number' | 'search' | 'url' | 'date' | 'time';
   value?: string;
   defaultValue?: string;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
