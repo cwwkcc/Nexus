579cbafc225967f6c72d311a7f9ef983f1fbdcaf
Chamila Roshana <cinderax@icloud.com>
2026-09-09 12:50:36 +0530

    feat: load .env in Next.js configs and filter required env vars

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
 // apps/web/next.config.js
+const path = require('node:path');
+const { loadEnvFile } = require('node:process');
+
+loadEnvFile(path.resolve(__dirname, '../../.env'));
+
+for (const key of ['GOOGLE_CLIENT_ID', 'GOOGLE_CLIENT_SECRET', 'SENTRY_DSN']) {
+  if (!process.env[key]) delete process.env[key];
+}
+
 const { createWebConfig } = require('@nexus/config/next');
 const { composePlugins, withNx } = require('@nx/next');
 const createNextIntlPlugin = require('next-intl/plugin');
